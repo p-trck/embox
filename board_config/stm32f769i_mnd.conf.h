d102ffd938640724b7ab23da5dbd49ab5c2107cb
p-trck <geektrck@gmail.com>
2023-10-12 17:39:30 +0900

    serial 출력 확인

---
+++ board_config/stm32f769i_mnd.conf.h
@@ -0,0 +1,65 @@
+#include <gen_board_conf.h>
+#include <stm32.h>
+
+struct uart_conf uarts[] = {
+	[1] = {
+		.status = DISABLED,
+		.name = "USART1",
+		.dev = {
+			.irqs = {
+				VAL("", 37),
+			},
+			.pins = {
+				PIN("TX", PB, PIN_14, AF4),
+				PIN("RX", PB, PIN_15, AF4),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOB),
+				VAL("RX",   CLK_GPIOB),
+				VAL("UART", CLK_USART1),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[2] = {
+		.status = DISABLED,
+		.name = "USART2",
+		.dev = {
+			.irqs = {
+				VAL("", 38),
+			},
+			.pins = {
+				PIN("TX", PD, PIN_5, AF7),
+				PIN("RX", PA, PIN_3, AF7),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOD),
+				VAL("RX",   CLK_GPIOA),
+				VAL("UART", CLK_USART2),
+			}
+		},
+		.baudrate = 115200,
+	},
+	[6] = {
+		.status = ENABLED,
+		.name = "USART6",
+		.dev = {
+			.irqs = {
+				VAL("", 71),
+			},
+			.pins = {
+				PIN("TX", PC, PIN_6, AF8),
+				PIN("RX", PC, PIN_7, AF8),
+			},
+			.clocks = {
+				VAL("TX",   CLK_GPIOC),
+				VAL("RX",   CLK_GPIOC),
+				VAL("UART", CLK_USART6),
+			}
+		},
+		.baudrate = 115200,
+	},
+};
+
+EXPORT_CONFIG(UART(uarts))
+
--- project/pjsip/templates/stm32f769i-mnd/board.conf.h
+++ project/pjsip/templates/stm32f769i-mnd/board.conf.h
@@ -1,4 +1,4 @@
-#include <stm32f769i_discovery.conf.h>
+#include <stm32f769i_mnd.conf.h>
 
 CONFIG {
 	uarts[1].status = ENABLED;
